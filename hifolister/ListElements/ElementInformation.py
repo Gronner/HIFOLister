35bd29cf05d41cbffbb19146b2c1b38358ca5977
Gronner <Gronner@users.noreply.github.com>
2019-03-02 00:02:17 +0100

    Fix formatting and description error

--- hifolister/ListElements/ElementInformation.py
+++ hifolister/ListElements/ElementInformation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-"""ListElements base implementation used for a simple FIFO list."""
+"""ElementInformation describing a ListElement."""
 
 class ElementInformation:
-    """Contains printable information about an element"""
+    """Contains printable information about an element."""
 
     def __init__(self, name, description=''):
         self.name = name
